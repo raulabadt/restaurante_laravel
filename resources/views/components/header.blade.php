850cf3d175d38a37a17b8ce4c4a14b90a8882252
Raúl Abad <raulabadtorralba@gmail.com>
2024-01-31 17:01:04 +0100

    Routes icon header.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,15 +5,15 @@
 // https://v0.dev/t/lzA0ifEb01r
 -->
 <header class="flex items-center justify-between px-4 py-2 bg-white dark:bg-red-800">
-  <a class="flex items-center" >
+  <a href="/" class="flex items-center" >
   <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chef-hat" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c1.918 0 3.52 1.35 3.91 3.151a4 4 0 0 1 2.09 7.723l0 7.126h-12v-7.126a4 4 0 1 1 2.092 -7.723a4 4 0 0 1 3.908 -3.151z" /><path d="M6.161 17.009l11.839 -.009" /></svg>
     <span class="sr-only">Acme Inc</span>
   </a>
 
   <nav class="flex items-center space-x-10">
-    <a class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="/">Nosotros</a>
-    <a class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="#">Para Llevar</a>
-    <a class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="#">Carta</a>
+    <a class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="/about">Nosotros</a>
+    <a class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="/take_away">Para Llevar</a>
+    <a class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="/menu">Carta</a>
     <a class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="/reserve">Reservas</a>
     <a class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="/contact">Contacto</a>
   </nav>
