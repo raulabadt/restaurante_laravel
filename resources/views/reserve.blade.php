3950760ec6419cdc5c74db316bc6ef8aa8e6245a
Raúl Abad <raulabadtorralba@gmail.com>
2024-03-04 14:09:48 +0100

    Update text-white is the same.

--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -72,7 +72,7 @@
                 <!-- </map>
             </div> -->
 
-            <button type="submit" class="flex items-center bg-orange-500 text-white gap-1 px-4 py-2 cursor-pointer text-gray-800 font-semibold tracking-widest rounded-md duration-300 hover:gap-2 hover:translate-x-3">
+            <button type="submit" class="flex items-center bg-orange-500 gap-1 px-4 py-2 cursor-pointer text-gray-800 font-semibold tracking-widest rounded-md duration-300 hover:gap-2 hover:translate-x-3">
                 Reservar
                 <svg class="w-5 h-5" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" stroke-linejoin="round" stroke-linecap="round"></path>
