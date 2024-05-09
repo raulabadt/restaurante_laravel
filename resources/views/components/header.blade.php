5a14b84360d2de2d93180b9839d178cd30cb3177
Raúl Abad <raulabadtorralba@gmail.com>
2024-05-09 13:12:14 +0200

    link about

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header class="flex items-center justify-between px-4 py-4 bg-orange-300 flex-wrap">
-  <a href="http://vps-c71468c0.vps.ovh.net/restaurante_laravel/public/" class="flex items-center">
+  <a href="http://vps-c71468c0.vps.ovh.net/restaurante_laravel/resource/views" class="flex items-center">
     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chef-hat" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c1.918 0 3.52 1.35 3.91 3.151a4 4 0 0 1 2.09 7.723l0 7.126h-12v-7.126a4 4 0 1 1 2.092 -7.723a4 4 0 0 1 3.908 -3.151z" /><path d="M6.161 17.009l11.839 -.009" /></svg>
     <span class="text-gray-700 ml-1 text-base md:text-lg">MI RESTAURANTE</span>
   </a>
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {
     return view('homepage');
 });
 
-Route::get('http://vps-c71468c0.vps.ovh.net/restaurante_laravel/resource/views/contact', function () {
+Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
