bbd4cd2c503075204b1825ceafa08f6290254b00
Raúl Abad <raulabadtorralba@gmail.com>
2024-05-09 13:09:24 +0200

    link about

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/', function () {
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
 Route::get('/whitelist', function () {
     return view('whitelist');
